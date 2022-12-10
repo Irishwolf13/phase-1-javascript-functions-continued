816532fb93fa9e52cc3dddb995b0d31bc6f1cedd
John Rooney <rooneyjohn@gmail.com>
2022-12-10 11:59:29 -0500

    commit tree 74cb6dd35924fdce65e65f451dc1ba551dbc13ab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(value = "roller-skate") {
+    return `This Saturday, I want to ${value}!`
+}
+
+const mondayWork = function (value = "go to the office") {
+    return `This Monday, I will ${value}.`
+}
+
+function wrapAdjective(visualFlair = "*") {
+    return function (value = 'special') {
+        return `You are ${visualFlair}${value}${visualFlair}!`
+    }
+}
